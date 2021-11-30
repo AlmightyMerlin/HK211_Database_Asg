ad5edefb8f8430bffd291d348f2261e63bbe15cb
maihoangkhai <maihoangkhai@outlook.com>
2021-11-30 15:50:33 +0700

    Fixed phone

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var homeController = require('./controllers/home');
 var facultyController = require("./controllers/faculty");
 var userController = require("./controllers/user");
 var phoneController = require("./controllers/phone");
-var phoneController = require("./controllers/manager");
+var managerController = require("./controllers/manager");
 
 var moment = require('moment');
 app.locals.moment = moment
@@ -25,7 +25,7 @@ app.use("/", homeController);
 app.use("/faculty", facultyController);
 app.use("/user", userController);
 app.use("/phone", phoneController);
-app.use("/manager", phoneController);
+app.use("/manager", managerController);
 
 app.listen(5000, function() {
     console.log("Server is listening on port 5000!");
